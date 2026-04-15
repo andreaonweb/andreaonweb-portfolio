690415bc7360b6c68e77f26725b98ef03417490c
andreaonweb <aoliverar.96@gmail.com>
2026-04-15 11:20:26 +0200

    feat: hacer todas las secciones responsive

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,29 @@
 import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class NavbarComponent {
   scrolled = false;
+  menuOpen = false;
 
   @HostListener('window:scroll')
   onScroll() {
     this.scrolled = window.scrollY > 40;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.style.overflow = this.menuOpen ? 'hidden' : '';
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.style.overflow = '';
   }
 }
